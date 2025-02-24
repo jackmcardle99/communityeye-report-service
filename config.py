7691649a95138615e16b649df233c45d13d0de0a
Jack <grutto.dev@proton.me>
2025-02-24 14:11:38 +0000

    refactored report service, added logging

---
+++ config.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from pymongo import MongoClient
+import os
+
+
+load_dotenv()
+
+
+# MongoDB config
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
+MONGO_COLLECTION_REPORTS = os.getenv("MONGO_COLLECTION_REPORTS")
+MONGO_COLLECTION_AUTHORITIES = os.getenv("MONGO_COLLECTION_AUTHORITIES")
+CLIENT = MongoClient(MONGO_URI)
+DB = CLIENT[MONGO_DB_NAME]
+
+
+# Flask config
+FLASK_SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
+FLASK_DEBUG = os.getenv("FLASK_DEBUG")
+FLASK_HOST = os.getenv("FLASK_HOST")
+FLASK_PORT = int(os.getenv("FLASK_PORT"))
+
+# Azure Blob Storage config
+AZURE_STORAGE_ACCOUNT = os.getenv("AZURE_STORAGE_ACCOUNT")
+AZURE_STORAGE_CONTAINER = os.getenv("AZURE_STORAGE_CONTAINER")
+AZURE_STORAGE_SAS = os.getenv("AZURE_STORAGE_SAS")
--- globals.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-
-client = MongoClient("mongodb://127.0.0.1:27017")
-db = client.communityeye_reports
-secret_key = ''
-
